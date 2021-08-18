94f4b63e3c84cc098c47de68bfd69df7bb5b9007
nepinney <nicholas.pinney80@gmail.com>
2021-08-17 23:00:35 -0400

    Added og:image and title to meta info

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,8 @@ export default {
       { hid: 'app', name: 'description', content: 'Sustainables promote environmentally concious products from a variety of brands. ' +
           'Products made from recycled materials, sourced or made locally, to products that aim to replace single use plastics. Our goal is to promote products and brands' +
           'that reduce our carbon footprint.' },
+      { property: 'og:title', content: 'Sustainables' },
+      { property: 'og:image', content: '/logo.png' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
